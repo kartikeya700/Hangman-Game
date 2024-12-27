31085f877fd2b2f2e60561ed62f61dd3af1183a4
kartikeya700 <kartikeyamittal@outlook.com>
2024-12-27 16:58:31 +0530

    Added more functionality to the game

--- hangman.py
+++ hangman.py
@@ -3,6 +3,7 @@ import random
 from hangman_words import word_list
 from hangman_art import logo, stages
 
+
 def main():
     lives = 6
 
@@ -21,16 +22,13 @@ def main():
     # defines flag which is used to stop game
     flag = 1
     correct_guess = []
+    guesses = []
 
     while flag:
         # takes input from user
         guess = input("Guess a letter: ").lower()
 
         display = ""
-        
-        # if user has already guessed the letter 
-        if guess in correct_guess:
-            print(f"You have already guessed {guess}")
 
         # checks if the guessed letter is in the chosen word and appends display string acc
         for letter in chosen_word:
@@ -42,25 +40,31 @@ def main():
             else:
                 display += "_"
 
-        # modifies number of lives for incorrect guess
-        if guess not in chosen_word:
-            lives -= 1
-            print(f"You guessed {guess}, that's not in the word. You lose a life.")
-            # if no lives remain end game
-            if lives == 0:
-                flag = 0
-                print(f"****************************IT WAS {chosen_word.upper()}! YOU LOSE****************************")
-                break
-        # prints the word after guess was made
+        # if user has already guessed the letter
+        if guess in guesses:
+            print(f"You have already guessed {guess}")
         else:
-            print(display)
+            # modifies number of lives for incorrect guess
+            if guess not in chosen_word:
+                lives -= 1
+                guesses.append(guess)
+                print(f"You guessed {guess}, that's not in the word. You lose a life.")
+                # if no lives remain ends game
+                if lives == 0:
+                    flag = 0
+                    print(f"****************************IT WAS {chosen_word.upper()}! YOU LOSE****************************")
+                    break
+            # to print the new word to guess if guess was correct
+            else:
+                guesses.append(guess)
+                print(display)
 
-        if "_" not in display:
-            flag = 0
-            print("****************************YOU WIN!****************************")
-        
-        # prints the hangman stage, number of lives remaining and new word to guess
-        print(stages[lives])
+            if "_" not in display:
+                flag = 0
+                print("****************************YOU WIN!****************************")
+
+            print(stages[lives])
+
         print(f"****************************{lives}/6 LIVES LEFT****************************")
         print("Word to guess: " + display)
 
