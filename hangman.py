2c62e8237cfb23ea35bbfa5630b93245f6cad623
kartikeya700 <kartikeyamittal@outlook.com>
2024-12-27 17:12:21 +0530

    Added input checking functionality to game

--- hangman.py
+++ hangman.py
@@ -21,8 +21,9 @@ def main():
 
     # defines flag which is used to stop game
     flag = 1
+    # stores correct and incorrect guesses made by user
     correct_guess = []
-    guesses = []
+    incorrect_guess = []
 
     while flag:
         # takes input from user
@@ -30,34 +31,38 @@ def main():
 
         display = ""
 
+        # checks if input is valid or not
+        if not guess.isalpha() or len(guess) != 1:
+            print("Invalid input. Please enter a single alphabetic character.")
+            continue
+
         # checks if the guessed letter is in the chosen word and appends display string acc
         for letter in chosen_word:
             if letter == guess:
                 display += letter
-                correct_guess.append(guess)
             elif letter in correct_guess:
                 display += letter
             else:
                 display += "_"
 
         # if user has already guessed the letter
-        if guess in guesses:
+        if guess in incorrect_guess or guess in correct_guess:
             print(f"You have already guessed {guess}")
         else:
             # modifies number of lives for incorrect guess
             if guess not in chosen_word:
                 lives -= 1
-                guesses.append(guess)
+                incorrect_guess.append(guess)
                 print(f"You guessed {guess}, that's not in the word. You lose a life.")
                 # if no lives remain ends game
                 if lives == 0:
                     flag = 0
                     print(f"****************************IT WAS {chosen_word.upper()}! YOU LOSE****************************")
-                    break
+                    continue
             # to print the new word to guess if guess was correct
             else:
-                guesses.append(guess)
                 print(display)
+                correct_guess.append(guess)
 
             if "_" not in display:
                 flag = 0
